e8c52579e916d619a036bfa50dfcef28d1464f9a
alanp1995 <Alan.Philpott@students.ittralee.ie>
2017-03-05 20:10:54 +0000

    Lecture 12 exercise completed on SharedPreferences, only validation left to complete

---
+++ SharedPreferences/app/src/main/java/phil/alan/sharedpreferences/SecondActivity.java
@@ -0,0 +1,26 @@
+package phil.alan.sharedpreferences;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button backButton = (Button) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, EnterNameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
