1f23f4f0c8f42bc61dc63c9b656c7c19563ddebd
alanp1995 <alan.philpott@students.ittralee.ie>
2017-04-26 13:58:05 +0100

    Project Started

---
+++ CA Project/Doshwise/app/src/main/java/phil/alan/doshwise/create_household.java
@@ -0,0 +1,13 @@
+package phil.alan.doshwise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class create_household extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_household);
+    }
+}
